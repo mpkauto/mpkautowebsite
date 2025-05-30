b7db5f6d557d814d3ddcc568c8529a10c91f279b
mpkauto <your-mpkauto-email@example.com>
2025-05-30 12:32:16 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,19 +5,18 @@ import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// https://vitejs.dev/config/
 export default defineConfig({
   root: path.resolve(__dirname, 'client'),
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'client/src'),
-      '@shared': path.resolve(__dirname, 'shared'),
-      '@assets': path.resolve(__dirname, 'attached_assets'),
-    },
+      '@shared': path.resolve(__dirname, 'client/src/shared'),
+      '@assets': path.resolve(__dirname, 'client/attached_assets')
+    }
   },
   build: {
     outDir: path.resolve(__dirname, 'dist'),
-    emptyOutDir: true,
-  },
+    emptyOutDir: true
+  }
 });
